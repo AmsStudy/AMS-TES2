814f6808e23bd42dba3f91ebbbca697d27b76915
AmsStudy <amsstudy@gmail.com>
2025-01-01 13:20:44 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -7,4 +7,9 @@
 </head>
 <body>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem sint voluptatem quasi ratione. Excepturi ipsa odio delectus blanditiis praesentium porro laudantium molestiae officia reiciendis officiis nemo quidem quia, tempore fuga.</p>
-</html>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
+    </html>
